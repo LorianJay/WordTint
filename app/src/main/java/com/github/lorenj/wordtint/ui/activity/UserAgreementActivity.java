53134c132fa90b1635794b7361879c74df255863
sukidayo <2985384723@qq.com>
2026-02-07 17:32:46 +0800

    用户协议

---
+++ app/src/main/java/com/github/lorenj/wordtint/ui/activity/UserAgreementActivity.java
@@ -0,0 +1,16 @@
+package com.github.lorenj.wordtint.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.lorenj.wordtint.R;
+
+public class UserAgreementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_agreement);
+    }
+}
--- app/src/main/java/com/github/lorenj/wordtint/ui/markdown/handler/APPLinkResolver.java
+++ app/src/main/java/com/github/lorenj/wordtint/ui/markdown/handler/APPLinkResolver.java
@@ -1,10 +1,12 @@
 package com.github.lorenj.wordtint.ui.markdown.handler;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.navigation.Navigation;
 
 import io.noties.markwon.LinkResolver;
 import io.noties.markwon.LinkResolverDef;
@@ -19,12 +21,23 @@ public class APPLinkResolver extends LinkResolverDef {
     @Override
     public void resolve(@NonNull View view, @NonNull String link) {
         // 如果是APP内自定义的语法,则跳转到目标页面
-        if (link.startsWith("navigation:")) {
-            String navigationAction = link.replaceFirst("navigation:", "");
-            Log.d(String.valueOf(this.getClass()), "跳转的目标页面navigationAction:" + navigationAction);
-            int id = view.getContext().getResources().getIdentifier(navigationAction, "id", view.getContext().getPackageName());
-            Navigation.findNavController(view).navigate(id, null);
-            return;
+        if (link.startsWith("activity:")) {
+            String targetActivity = link.replaceFirst("activity:", "");
+            Log.d(String.valueOf(this.getClass()), "跳转的目标页面activity:" + targetActivity);
+            Context context = view.getContext();
+            String packageName = context.getPackageName();
+            Class<?> clazz = null;
+            try {
+                clazz = Class.forName(packageName + ".ui.activity." + targetActivity);
+                Intent intent = new Intent(context, clazz);
+                if (!(context instanceof Activity)) {
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                }
+                context.startActivity(intent);
+                return;
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
         super.resolve(view, link);
     }
